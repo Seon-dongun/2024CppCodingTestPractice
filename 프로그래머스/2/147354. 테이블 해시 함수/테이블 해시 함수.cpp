0671c6d919d171f1dabd5cb28700e66910cc6d0f
Seon-dongun <77486690+Seon-dongun@users.noreply.github.com>
2024-02-20 16:47:49 +0900

    Update 테이블 해시 함수.cpp

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cpp
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cpp
@@ -1,15 +1,13 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 using namespace std;
 
-
-int t;
+int column;
 bool cmp(vector<int> A, vector<int> B){
-    if(A[t]>B[t])
+    if(A[column]>B[column])
         return false;
-    else if(A[t]<B[t])
+    else if(A[column]<B[column])
         return true;
     else{
         if(A[0]>B[0])
@@ -19,32 +17,17 @@ bool cmp(vector<int> A, vector<int> B){
     }
 }
 
-int cal(vector<int> sum,int n){
-    if(sum.size()-2==n)
-        return sum[n] ^ sum[n+1]; 
-        
-    return sum[n] ^ cal(sum,n+1);    
-}
-
-
 int solution(vector<vector<int>> data, int col, int row_begin, int row_end) {
     int answer = 0;
-    vector<int> sum;
-    t = col-1;
+    column = col-1;
     sort(data.begin(),data.end(),cmp);
     
     for(int i=row_begin-1;i<row_end;i++){
         int tmp=0;
-        for(int j=0;j<data[i].size();j++){
+        for(int j=0;j<data[i].size();j++)
             tmp+=(data[i][j]%(i+1));
-        }
-        sum.push_back(tmp);
+        answer ^= tmp;
     }
-    if(sum.size()==1)
-        answer = sum[0];
-    else
-        answer = cal(sum,0);
-    
     
     return answer;
 }
