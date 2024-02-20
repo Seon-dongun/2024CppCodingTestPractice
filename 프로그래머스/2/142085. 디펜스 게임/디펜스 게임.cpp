ad1f0151088a306a1cdca127ae5ebcf7a0d3b125
Seon-dongun <77486690+Seon-dongun@users.noreply.github.com>
2024-02-20 18:30:41 +0900

    Update 디펜스 게임.cpp

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.cpp
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.cpp
@@ -15,10 +15,10 @@ int solution(int n, int k, vector<int> enemy) {
         sum+=enemy[i];
         pq.push(enemy[i]);
         
-        if(k==0 && sum>n)
+        if(k==0 && sum>n) // 무적권을 모두 소모한 상태에서 막을 수 있는 한도를 넘었을 때
           break;
 
-        while(!pq.empty() && k!=0 && sum>n){
+        while(!pq.empty() && k!=0 && sum>n){ // 무적권이 남은 상태에서 막을 수 있는 한도를 넘었을 때. 한도 이내로 들어올 때까지 무적권을 최대한 소모하여 처리한다.
             k--;
             sum-=pq.top();
             pq.pop();
