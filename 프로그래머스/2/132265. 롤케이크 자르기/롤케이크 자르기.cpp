191ee32bc6a9913cfca192b90a315b57c75b71de
Seon-dongun <77486690+Seon-dongun@users.noreply.github.com>
2024-02-25 18:06:38 +0900

    Update 롤케이크 자르기.cpp

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -5,30 +5,19 @@ using namespace std;
 
 int solution(vector<int> topping) {
     int answer = 0;
-    map<int,int> A;
-    map<int,int> B;
-    for(int i=0;i<topping.size();i++){
-        if(A.find(topping[i]) == A.end())
-            A.insert({topping[i],1});
-        else
-            A[topping[i]]++;
-    }
+    map<int, int> m1;
+    map<int, int> m2;
+    for (int i : topping)
+        m2[i]++;
     
-    for(int i=0;i<topping.size();i++){
-        if(A.find(topping[i])!=A.end()){
-            A[topping[i]]--;
-            if(A[topping[i]]==0)
-                A.erase(topping[i]);
-            
-            if(B.find(topping[i]) == B.end())
-                B.insert({topping[i],1});
-            else
-                B[topping[i]]++;      
-        }
-           
-        if(A.size()==B.size())
+    for (int i = 0; i < topping.size() - 1; i++) {
+        m1[topping[i]]++;
+        m2[topping[i]]--;
+        if (m2[topping[i]] == 0)
+            m2.erase(topping[i]);
+        
+        if (m1.size() == m2.size()) 
             answer++;
     }
-  
     return answer;
 }
