04e5254ce9f30f2e77160200c0ea8e215444dd69
Seon-dongun <77486690+Seon-dongun@users.noreply.github.com>
2024-02-28 16:57:54 +0900

    Update n＾2 배열 자르기.cpp

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
@@ -7,12 +7,10 @@ vector<int> solution(int n, long long left, long long right) {
     vector<int> answer;
 
     for(long long i=left;i<=right;i++){
-        long long col = i/n;
-        long long row = i%n;
+        int row = i/n;
+        int col = i%n;
         
-        if(col==0 && row==0)
-            answer.push_back(1); 
-        else if(col==row)
+        if(col==row)
             answer.push_back(col+1);
         else{
             int minV = min(col,row);
