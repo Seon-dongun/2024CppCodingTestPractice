092d96c5d9e66a00451e6036ff50646b067f5eb0
Seon-dongun <77486690+Seon-dongun@users.noreply.github.com>
2024-03-03 20:48:07 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 30.54 ms, Memory: 27.9 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
@@ -6,10 +6,15 @@ vector<long long> solution(vector<long long> numbers) {
    vector<long long> answer;
     for (long long number : numbers)
     {
+        if(number%2==0){
+            answer.push_back(number+1);
+            continue;;
+        }
+        
         long long bit = 1;
         while ((number & bit) > 0)
             bit <<= 1;   
-        answer.push_back(number + bit - (bit >> 1));        
+        answer.push_back(number + (bit >> 1));        
     }
     return answer;
 }
