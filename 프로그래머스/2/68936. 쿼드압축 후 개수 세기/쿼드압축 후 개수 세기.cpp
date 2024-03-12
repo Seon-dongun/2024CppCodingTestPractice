699c38f3b6cdf954e101e07e4180453631379529
Seon-dongun <77486690+Seon-dongun@users.noreply.github.com>
2024-03-12 20:30:57 +0900

    Update 쿼드압축 후 개수 세기.cpp

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.cpp
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.cpp
@@ -30,9 +30,10 @@ void start(vector<vector<int>>& arr,int len,pair<int,int> loc){
     else if(ztmp==len*len)
         zero++;
     else{
-        pair<int,int> locations[4] = {{loc.first,loc.second},{loc.first,loc.second + len/2},{loc.first+len/2,loc.second},{loc.first+len/2,loc.second + len/2}};
-        for(int i=0;i<4;i++)
-            start(arr,len/2,locations[i]);
+        start(arr,len/2,{loc.first,loc.second});
+        start(arr,len/2,{loc.first,loc.second + len/2});
+        start(arr,len/2,{loc.first+len/2,loc.second});
+        start(arr,len/2,{loc.first+len/2,loc.second + len/2});
     }
 }
 
