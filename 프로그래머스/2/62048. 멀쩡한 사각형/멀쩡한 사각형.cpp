b0a85a9dfeb7fd5b54b7d9f4557f26ed2eedb15e
Seon-dongun <77486690+Seon-dongun@users.noreply.github.com>
2024-03-15 21:30:53 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.cpp
@@ -0,0 +1,11 @@
+#include <cmath>
+using namespace std;
+
+int gcd(int a, int b)
+{
+    return b ? gcd(b, a % b) : a;
+} 
+
+long long solution(int w,int h) {
+    return (long long)w * (long long)h - (w+h) + gcd(w,h);
+}
