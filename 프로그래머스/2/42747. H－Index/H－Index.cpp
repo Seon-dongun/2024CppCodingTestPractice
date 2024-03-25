1964b45b7f1ecf01fabab14fc3cf5d86451c2a8f
Seon-dongun <77486690+Seon-dongun@users.noreply.github.com>
2024-03-25 18:11:36 +0900

    Update H－Index.cpp

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -1,47 +1,20 @@
-#include <string>
 #include <vector>
-
+#include <algorithm>
+ 
 using namespace std;
-
+ 
 int solution(vector<int> citations) {
     int answer = 0;
+    sort(citations.begin(), citations.end(), greater<int>());
     
-    for(int i=0;i<citations.size();i++){
-        int h = i+1;
-        int flag1 = 0, flag2 = 1;
-        int cnt1 = 0, cnt2 = 0;
-        int s = 0,flag3=0;
-        for(int j=0;j<citations.size();j++){
-            if(citations[j]>=h)
-                cnt1++;      
-            
-            if(cnt1==1&&flag3==0){
-                s = j;
-                flag3 = 1;
-            }
-            
-            if(cnt1==h)
-                flag1=1;
-        }
-        
-        if(flag1==1){
-            for(int j=s-1;j>=0;j--){
-                if(citations[j]>h){
-                    flag2 = 0;
-                    break;
-                }
-            }
-            if(flag1 && flag2){
-                answer = max(answer,h);
-            }
-            
-        }
-        
-        
-        
+    if (!citations[0])        
+        return 0;
+    
+    for (int i = 0; i < citations.size(); i++){
+        if (citations[i] > i)    
+            answer++;
+        else    
+            break;
     }
-    
-    return answer;
+    return answer+1;
 }
-
-//0 1 3 5 6
