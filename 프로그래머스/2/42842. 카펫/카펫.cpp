2f14c4395ef9f974fe6b9afe00f4d192c5ac6b83
Seon-dongun <77486690+Seon-dongun@users.noreply.github.com>
2024-03-25 16:14:11 +0900

    Update 카펫.cpp

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -5,11 +5,10 @@ using namespace std;
 
 vector<int> solution(int brown, int yellow) {
     vector<int> answer;
-    int check = yellow+brown;
     int i=3;
-    while(i>=3){
+    while(1){
         for(int j=3; j<=i;j++){
-            if(i*j == check && ((i-2)*(j-2) == yellow) && (2*i + 2*j + -4 == brown)){
+            if(((i-2)*(j-2) == yellow) && (2*i + 2*j + -4 == brown)){
                 answer.push_back(i);
                 answer.push_back(j);
                 return answer;
@@ -19,8 +18,8 @@ vector<int> solution(int brown, int yellow) {
     }
 }
 
-// x-2 * y-2 = yellow => xy - 2y - 2x +4 = yellow
-// x*2 + (y-2)*2 = brown => - 2x - 2y+4 = - brwon
+// x-2 * y-2 = yellow 
+// x*2 + (y-2)*2 = brown
 
-//xy - 2y -2x +4 = yellow => xy - brown = yellow => xy = yellow + brwon;
+
 
