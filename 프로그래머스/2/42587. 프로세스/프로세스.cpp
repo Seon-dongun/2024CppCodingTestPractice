3925728cfeb6285b894839a8d44159d7cae6f0ac
Seon-dongun <77486690+Seon-dongun@users.noreply.github.com>
2024-03-26 18:03:40 +0900

    [level 2] Title: 프로세스, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+    int answer = 0;
+    queue<pair<int,int>> q;  // {우선순위, 인덱스}
+    priority_queue<int> pq(priorities.begin(), priorities.end());
+    
+    for(int i = 0; i < priorities.size(); i++)
+        q.push({priorities[i], i});
+    
+    while(!q.empty()){
+        int curPriority = q.front().first;
+        int curIndex = q.front().second;
+        q.pop();
+        
+        if(curPriority < pq.top()){
+            q.push({curPriority, curIndex});
+        }
+        else{
+            pq.pop();
+            answer++;
+            if(curIndex == location)
+                break;
+        }
+    }
+    
+    return answer;
+    
+
+}
