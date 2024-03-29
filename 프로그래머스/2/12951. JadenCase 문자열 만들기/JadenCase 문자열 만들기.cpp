66a637550c8acb39a7bdd9b3f7d5579aee5b484d
Seon-dongun <77486690+Seon-dongun@users.noreply.github.com>
2024-03-29 11:20:52 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    bool inside = false;
+    
+    for(auto c : s) {
+        if (c == ' ') {
+            answer += ' ';
+            inside = false;
+        } else if (!inside) {
+            answer += toupper(c);
+            inside = true;
+        } else {
+            answer += tolower(c);
+        }
+    }
+    return answer;
+}
