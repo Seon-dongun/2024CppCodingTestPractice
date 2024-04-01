f46513ccfa6812ff1ec18a8a6d1dea435f2fa9e0
Seon-dongun <77486690+Seon-dongun@users.noreply.github.com>
2024-04-01 19:48:53 +0900

    Update N개의 최소공배수.cpp

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
@@ -1,25 +1,19 @@
-#include <string>
 #include <vector>
 
 using namespace std;
 
-int check(int a,int b){
-    int n = a;
-    int m = b;
-    while(1){
-        if(n==m)
-            return n;      
-        if(n<m)
-            n+=a;
-        else if(n>m)
-            m+=b;
-    }
+int gcd(int a, int b) {
+    return b==0 ? a : gcd(b, a % b);
+}
+
+int lcm(int a, int b) {
+    return (a * b) / gcd(a, b);
 }
 
 int solution(vector<int> arr) {
     int answer = 0;
     for(int i=0;i<arr.size()-1;i++){
-        answer = check(arr[i],arr[i+1]);
+        answer = lcm(arr[i],arr[i+1]);
         arr[i+1] = answer;
     }
     return answer;
