097c7822d5da6baf5b8d0a481ba8f4fbe2559573
Seon-dongun <77486690+Seon-dongun@users.noreply.github.com>
2024-04-03 16:03:07 +0900

    Update 행렬의 곱셈.cpp

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cpp
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cpp
@@ -1,4 +1,3 @@
-#include <string>
 #include <vector>
 
 using namespace std;
@@ -8,17 +7,16 @@ vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2)
     
     for(int i=0;i<arr1.size();i++){
         vector<int> v;
-        int tmp = 0;
+        
         for(int k=0;k<arr2[0].size();k++){
-            for(int j=0;j<arr1[i].size();j++){
-                tmp += arr1[i][j] * arr2[j][k];
-            }
+            int tmp = 0;
+            for(int j=0;j<arr1[i].size();j++)
+                tmp += arr1[i][j] * arr2[j][k];    
             v.push_back(tmp);
             tmp = 0;
         }
         answer.push_back(v);
     }
-    
     return answer;
 }
 
