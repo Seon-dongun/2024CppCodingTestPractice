bf81e341eca55c8c93457d57558eeb08c5bc2f12
Seon-dongun <77486690+Seon-dongun@users.noreply.github.com>
2024-04-08 17:37:08 +0900

    Update 땅따먹기.cpp

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
@@ -14,15 +14,13 @@ int solution(vector<vector<int> > land)
     
     for(int i=1;i<n;i++){
         for(int j=0;j<4;j++){
-            int max = 0;
             for(int k=0;k<4;k++){
                 if(j==k)
                     continue;
                 
                 if(max < land[i][j] + dp[i-1][k])
-                    max = land[i][j] + dp[i-1][k];             
+                    dp[i][j] = max(dp[i][j],land[i][j] + dp[i-1][k]);             
             }
-            dp[i][j] = max;
         }
     }
     
