87000b83d9f78fc29f6ea26a2034d3edf12be574
Seon-dongun <77486690+Seon-dongun@users.noreply.github.com>
2024-05-03 17:35:15 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int dp[11];
+    cin >> n;
+
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+    for (int i = 4; i <= 10; i++)
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+
+
+
+    while (n--) {
+        int num;
+        cin >> num;
+        cout << dp[num] << endl;
+    }
+    return 0;
+}
