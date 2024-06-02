ef62183a0f56174e3fc1082ec40bd6983ae67434
Seon-dongun <77486690+Seon-dongun@users.noreply.github.com>
2024-06-02 17:56:30 +0900

    [Gold IV] Title: 테트로미노, Time: 80 ms, Memory: 4392 KB -BaekjoonHub

---
+++ 백준/Gold/14500. 테트로미노/테트로미노.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int board[551][551];
+int visited[551][551];
+int answer = 0;
+int n, m;
+int dx[4] = { 0,0,1,-1 };
+int dy[4] = { 1,-1,0,0 };
+void dfs(int x, int y, int cnt, int sum) {
+    if (cnt == 4) {
+        answer = max(answer, sum);
+        return;
+    }
+    for (int i = 0; i < 4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        if (1 <= nx && nx <= n && 1 <= ny && ny <= m && visited[nx][ny]==0) {
+            visited[nx][ny] = 1;
+            dfs(x + dx[i], y + dy[i], cnt + 1, sum + board[nx][ny]);
+            visited[nx][ny] = 0;
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            cin >> board[i][j];
+        }
+    }
+
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            visited[i][j] = 1;
+            dfs(i, j, 1, board[i][j]);
+            visited[i][j] = 0;
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            int tmp = board[i][j] + board[i - 1][j] + board[i + 1][j] + board[i][j - 1] + board[i][j + 1];
+            tmp -= min({ board[i - 1][j],board[i + 1][j],board[i][j - 1],board[i][j + 1] });
+            answer = max(answer, tmp);
+        }
+    }
+
+    cout << answer;
+    return 0;
+}
