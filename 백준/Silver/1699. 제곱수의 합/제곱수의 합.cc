c7e21c875871406a31aef87cffbc5c607f2e10db
Seon-dongun <77486690+Seon-dongun@users.noreply.github.com>
2024-06-18 17:44:47 +0900

    Update 제곱수의 합.cc

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
@@ -14,6 +14,7 @@ int main() {
     for(int i=1; i<=n; i++){
         for(int j = 1; j*j<=i; j++){
             dp[i] = min(dp[i], dp[i-j*j]+1);
+            // dp[i-j*j]+1에서 +1은 j*j로 만들어지는 제곱수는 무조건 1개의 항이기 때문에 1임.
         }
     }
 
