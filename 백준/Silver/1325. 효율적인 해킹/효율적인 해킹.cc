6216f51f8878b4afa0dc0742f59509625af82fa9
Seon-dongun <77486690+Seon-dongun@users.noreply.github.com>
2024-07-30 16:17:04 +0900

    [Silver I] Title: 효율적인 해킹, Time: 2736 ms, Memory: 3648 KB -BaekjoonHub

---
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <set>
+#include <cstring>
+using namespace std;
+
+vector<int> graph[10001];
+int value[10001];
+int visited[10001] = { 0, };
+
+set<int> answer;
+
+int maxCnt = -1;
+void check(int id){     
+    int cnt = 1;
+    queue<int> q;
+    visited[id] = 1;
+    q.push(id);
+
+    while (!q.empty()) {
+        int cid = q.front();
+        q.pop();
+
+        for (int i = 0; i < graph[cid].size(); i++) {
+            int nid = graph[cid][i];
+            if (visited[nid] == 0) {
+                visited[nid] = 1;
+                q.push(nid);
+                cnt++;
+            }
+        }
+    }
+
+    if (cnt > maxCnt) {
+        maxCnt = cnt;
+        answer.clear();
+    }
+
+    if(cnt==maxCnt)
+        answer.insert(id);
+}
+
+
+int main() {
+    int n, m, a, b;
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b;
+        graph[b].push_back(a);
+    }
+
+    for (int i = 1; i <= n; i++) {
+        memset(visited, 0, sizeof(visited));
+        check(i);
+    }
+
+    for (auto& tmp : answer)
+        cout << tmp << " ";
+    return 0;
+}
