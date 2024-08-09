37bb23a37275a3ae9028288e5929102298f2b686
Seon-dongun <77486690+Seon-dongun@users.noreply.github.com>
2024-08-09 16:16:19 +0900

    [Silver II] Title: 창고 다각형, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using namespace std;
+vector<pair<int, int>> v;
+
+int main() {
+    int n, x, y;
+    cin >> n;
+    pair<int, int> maxPos = { -1,-1 };
+    for (int i = 0; i < n; i++) {
+        cin >> x >> y;
+        v.push_back({ x,y });
+        if (maxPos.second < y) {
+            maxPos.first = x;
+            maxPos.second = y;
+        }
+    }
+
+    sort(v.begin(), v.end());
+
+    int sum = 0;
+    int tmpx = v[0].first;
+    int tmpy = v[0].second;
+    int i = 0;
+
+    while (i<n) {
+        if (tmpy <= v[i].second) {
+            sum += ((v[i].first - tmpx) * tmpy);
+            tmpx = v[i].first;
+            tmpy = v[i].second;
+        }
+
+
+        i++;
+    }
+
+    i = n - 1;
+    tmpx = v[i].first;
+    tmpy = v[i].second;
+
+    while (i>=0) {
+        if (tmpy < v[i].second) {
+            sum += ((tmpx-v[i].first) * tmpy);
+            tmpx = v[i].first;
+            tmpy = v[i].second;
+        }
+
+        i--;
+    }
+
+    cout << sum+maxPos.second << endl;
+    return 0;
+}
